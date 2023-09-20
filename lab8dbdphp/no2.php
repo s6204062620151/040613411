bbbd676d2b0cbe05cf1de5221f35f732ac9d680b
s6204062620151 <s6204062620151@email.kmutnb.ac.th>
2023-09-20 14:23:06 +0700

    workshop 2

---
+++ lab8dbdphp/no2.php
@@ -0,0 +1,14 @@
+<?php include "connect.php" ?>
+<html>
+<body>
+    <?php
+        $stmt = $pdo->prepare("SELECT * FROM member");
+        $stmt->execute(); 
+        while ($row=$stmt->fetch()) :       
+        echo "ชื่อสมาชิก : " . $row["name"] . "<br>";
+        echo "ที่อยู่ : " . $row["address"] . "<br>";
+        echo "อีเมล : " . $row["email"] . "<br><hr>";
+        endwhile;
+    ?>
+</body>
+</html>
