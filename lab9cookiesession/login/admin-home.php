dd270388fdc75350e41fc6c8051765fa16ee199e
s6204062620151 <s6204062620151@email.kmutnb.ac.th>
2023-10-04 15:15:08 +0700

    track 1,2 finsih

--- lab9cookiesession/login/admin-home.php
+++ lab9cookiesession/login/admin-home.php
@@ -1,17 +1,23 @@
-<?php session_start(); ?>
-
+<?php
+	include "connect.php";
+    session_start();
+    // ตรวจสอบว่ามีชื่อใน session หรือไม่ หากไม่มีให้ไปหน้า login อัตโนมัติ
+    if (empty($_SESSION["username"]) ) { 
+        header("location: login-form.php");
+    }
+?>
 <html>
 <body>
 <h1>สวัสดี ADMIN: <?=$_SESSION["fullname"]?></h1>
 หากต้องการออกจากระบบโปรดคลิก <a href='logout.php'>ออกจากระบบ</a>
 <h1>รายการสั่งซื้อ</h1>
 <?php
-    $stmt = $pdo->prepare("SELECT username,COUNT(ord_id) as order_amount FROM orders GROUP BY username");
+    $stmt = $pdo->prepare("SELECT username,COUNT(ord_id) FROM orders GROUP BY username");
     $stmt->execute();
 
     while ($row = $stmt->fetch()) {
         echo "ไอดีผู้ใช้: " . $row ["username"] . "<br>";
-        echo "จำนวนคำสั่งซื้อ: " . $row ["ord_id"] . "<br>";
+        echo "จำนวนคำสั่งซื้อ: " . $row ["COUNT(ord_id)"] . "<br>";
         echo "<hr>\n";
     }
 ?>
